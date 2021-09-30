28b1577ffcfc983eae23d37c9bac9da95146a333
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2021-09-30 12:58:09 -0300

    chore(tsconfig): skip node_modules lib types check

--- src/atoms/Colors/Colors.tsx
+++ src/atoms/Colors/Colors.tsx
@@ -3,8 +3,10 @@ import { SimpleGrid, Box, BoxProps } from '@chakra-ui/react'
 import { colors } from '../../theme/colors'
 import { Color } from './Color'
 
+type ColorsType = keyof typeof colors
+
 export interface ColorsProps extends BoxProps {
-  type: 'main' | 'alert' | 'icon' | 'neutral'
+  type: ColorsType
 }
 
 export function Colors({ type, ...styles }: ColorsProps): JSX.Element {
@@ -12,7 +14,13 @@ export function Colors({ type, ...styles }: ColorsProps): JSX.Element {
     <Box {...styles}>
       <SimpleGrid columns={6} spacing={5} minChildWidth="8rem">
         {Object.keys(colors[type]).map((color: string, index) => (
-          <Color key={index} type={type} color={color} hexa={colors[type][color]} />
+          <Color
+            key={index}
+            type={type}
+            color={color}
+            // @ts-expect-error
+            hexa={colors[type][color]}
+          />
         ))}
       </SimpleGrid>
     </Box>
