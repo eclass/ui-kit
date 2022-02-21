8e2f2a4d98b1691700785ba3ba1c668538ea9ce4
eclass <eclass@MacBook-Air-de-eclass.local>
2022-02-21 17:51:20 -0300

    feat(theme): sombras

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -4,13 +4,14 @@ import { colors } from './colors'
  * Valores definidos en el sistema de diseños eclass.
  * @see https://zeroheight.com/15698077d/p/48fae4-eclass-sistema-de-diseo
  */
-const base = 'solid 1px'
+const base = 'solid .063rem'
 export const borders = {
-  normal: `${base} ${colors.neutral.silverSand}`,
+  normal: `${base} ${colors.neutral.darkCharcoal}`,
   light: `${base} ${colors.neutral.platinum}`,
   active: `${base} ${colors.main.deepSkyBlue}`,
   success: `${base} ${colors.alert.jadeGreen}`,
   error: `${base} ${colors.alert.red}`,
+  white: `solid .125rem ${colors.neutral.white}`,
 }
 
 /**
@@ -23,7 +24,18 @@ export const radii = {
   rounded: '50%',
 }
 
+/**
+ * Valores definidos en el sistema de diseños eclass.
+ * @see https://zeroheight.com/15698077d/p/787e81-shadows--elevation
+ */
+export const shadows = {
+  sm: '0 .125rem .5rem 0 rgba(0,0,0,.20)',
+  md: '0 .25rem 1rem 0 rgba(0,0,0,.20)',
+  lg: '0 .5rem 2rem 0 rgba(0,0,0,.20)',
+}
+
 export const utils = {
   borders,
   radii,
+  shadows,
 }
