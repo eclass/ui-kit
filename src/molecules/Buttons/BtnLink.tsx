adead28fae4e5818e41e85ce5f6f6f4815cbcf0c
Javiera Munita <munitajaviera@gmail.com>
2022-07-20 11:46:39 -0400

    feat(molecules): botón link sin ícono para sección 'mi perfil'

---
+++ src/molecules/Buttons/BtnLink.tsx
@@ -0,0 +1,37 @@
+import { vars } from '@theme'
+import { Box } from '@chakra-ui/react'
+
+export interface props {
+  children?: React.ReactNode
+  m?: string
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void
+  id?: string
+}
+
+export function BtnLink({ children, m = '0', onClick, id }: props): JSX.Element {
+  return (
+    <Box
+      as="button"
+      id={id}
+      width="fit-content"
+      padding=".25em"
+      fontFamily="Roboto"
+      fontStyle="normal"
+      fontWeight="500"
+      fontSize=".875rem"
+      lineHeight="1rem"
+      textDecorationLine="underline"
+      color={vars('colors-main-deepSkyBlue')}
+      m={m}
+      onClick={onClick}
+      _hover={{
+        color: vars('colors-neutral-darkCharcoal'),
+        cursor: 'pointer',
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
+
+BtnLink.displayName = 'BtnLink'
