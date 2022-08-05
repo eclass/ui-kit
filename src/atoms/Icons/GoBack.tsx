eeb83aa7e4e9b40a24a9297fadb8a937edce37bc
Javiera Munita <95220695+JavieraMunita@users.noreply.github.com>
2022-08-05 10:35:49 -0400

    fix(icons): cambio en ancho de viewbox iconos password y go-back

--- src/atoms/Icons/GoBack.tsx
+++ src/atoms/Icons/GoBack.tsx
@@ -2,8 +2,8 @@ import { Base, BaseProps } from './Base'
 
 export function GoBack(props: BaseProps): JSX.Element {
   return (
-    <Base {...props} w="16px" h="16px" viewBox="0 0 16 16" title="goBack">
-      <path d="M9.92593 2L4 8L9.92593 14L12 11.9L8.14815 8L12 4.1L9.92593 2Z" fill="currentColor" />
+    <Base {...props} w="8px" h="16px" viewBox="0 0 8 16" title="goBack">
+      <path d="M5.92593 2L0 8L5.92593 14L8 11.9L4.14815 8L8 4.1L5.92593 2Z" fill="currentColor" />
     </Base>
   )
 }
