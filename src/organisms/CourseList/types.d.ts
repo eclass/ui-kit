15d38411ce5bb98ad584197360cd3db6bde6bcf5
Javiera Munita <95220695+JavieraMunita@users.noreply.github.com>
2023-08-02 16:34:39 -0400

    fix(courselist): descripción de props (#516)

--- src/organisms/CourseList/types.d.ts
+++ src/organisms/CourseList/types.d.ts
@@ -9,9 +9,26 @@ interface PaymentText {
 }
 
 export interface WrapperCoursesProps {
+  /** Array de cursos a desplegar */
   courses: AcademicList[]
+
+  /** Especificación de margen
+   * @example m: '0 0 0 16px'
+   */
   m?: string
+
+  /** Objeto con información acerca del estado de pago del curso, que incluye 'title', 'body' y 'buttonName'.
+   * @example
+   *
+   * modalPaymentTextArg = {
+   * title: 'Curso congelado por pagos pendientes'
+   * body: 'Para regularizar esta situación, comúnicate con ___'
+   * buttonName: 'Entendido'
+   * }
+   */
   modalPaymentText?: PaymentText
+
+  /** Especificación de tipo de caja curso a mostrar. Por default se muestra el tipo TRADITIONAL */
   typeBox?: 'TRADITIONAL' | 'TRADITIONAL_LANDSCAPE' | 'IMAGE_LARGE' | 'IMAGE_SMALL'
 }
 
