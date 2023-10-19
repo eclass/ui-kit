c8aef600af61dac34d49e43f5df8510120619e13
Eriold <Eriold@users.noreply.github.com>
2023-10-19 15:33:18 +0000

    deploy: 681e4192e1d34a9a842201aa7b331270962965fc

---
+++ assets/Ripples.d11ddba2.js
@@ -0,0 +1,2 @@
+import{R as p}from"./Close.99b12ef3.js";import{a as r,B as s}from"./index.1e592a92.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.d11ddba2.js.map
--- assets/ShadowsElevation.484d6541.js
+++
@@ -1,4 +0,0 @@
-import{B as e}from"./BoxCopy.7cb0df8b.js";import{j as a,F as r,a as s,M as d,b as t,v as o,C as m}from"./index.2d01e7c9.js";import{L as n}from"./ListComponent.ad6d1a84.js";const l=()=>a(r,{children:[s(d,{children:"Shadows \xA7 Elevation"}),s(t,{children:"Algunos de nuestros componentes necesitan separarse del resto de la interfaz para ello se dispone de las sombras, que est\xE1n reservadas para estos componentes (por ejemplo, men\xFA desplegable o modales)."}),a(n,{gap:"5rem",m:"3rem 0 !important",children:[s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-sm"),text:"shadows-sm"}),s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-md"),text:"shadows-md"}),s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-lg"),text:"shadows-lg"})]}),s(m,{text:`<Box boxShadow={vars('shadows-sm')} />
-<Box boxShadow={vars('shadows-md')} />
-<Box boxShadow={vars('shadows-lg')} />`})]});export{l as ViewShadowsElevation,l as default};
-//# sourceMappingURL=ShadowsElevation.484d6541.js.map
