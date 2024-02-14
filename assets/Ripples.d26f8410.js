acb86fcb0271e113a8f3c51e866d35a824527ad8
Eriold <Eriold@users.noreply.github.com>
2024-02-14 19:04:30 +0000

    deploy: c9f5628427a48b6e7fab37fbf3ca41ae962acb8f

--- assets/Ripples.d26f8410.js
+++
@@ -1,2 +0,0 @@
-import{R as p}from"./Close.28683cc2.js";import{a as r,B as s}from"./index.9de7fc2f.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
-//# sourceMappingURL=Ripples.d26f8410.js.map
