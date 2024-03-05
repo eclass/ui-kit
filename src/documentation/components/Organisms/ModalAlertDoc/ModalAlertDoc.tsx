e7ea0ab2750a1a764d309a9a2856c31b636c68d1
Daniel Montoya <daniel.montoya@eclass.cl>
2024-03-05 15:15:04 -0500

    fix(test): add modal info test responsive

--- src/documentation/components/Organisms/ModalAlertDoc/ModalAlertDoc.tsx
+++ src/documentation/components/Organisms/ModalAlertDoc/ModalAlertDoc.tsx
@@ -5,6 +5,7 @@ import { ModalAlertProps } from '@/organisms/ModalAlert/types'
 
 export function ModalAlertDoc(): JSX.Element {
   const [isOpen, setIsOpen] = useState(false)
+  const [isOpenInfo, setIsOpenInfo] = useState(false)
   const optionsModal: ModalAlertProps = {
     showModal: isOpen,
     typeAlert: 'success',
@@ -27,10 +28,42 @@ export function ModalAlertDoc(): JSX.Element {
       },
     ],
   }
+
+  const optionsModaInfol: ModalAlertProps = {
+    showModal: isOpenInfo,
+    typeAlert: 'info',
+    title: 'Información sobre tu nota',
+    description: (
+      <>
+        La razón por la que tu nota bajó en la evaluación podría ser que no entendiste completamente
+        el tema. Esto sucede porque si no comprendes bien el contenido, es difícil responder
+        correctamente a las preguntas. Es importante dedicar tiempo a estudiar y practicar para
+        asegurarte de tener un buen entendimiento del tema antes de la evaluación. <br />
+        <br />
+        La razón para la disminución de tu nota podría ser que se detectó que copiaste en tus
+        respuestas. Copiar no solo es deshonesto, sino que también muestra una falta de comprensión
+        y habilidad para resolver problemas por tu cuenta. Es importante recordar que la integridad
+        académica es fundamental y que es mejor esforzarse por entender y resolver los problemas por
+        uno mismo, incluso si eso significa obtener una nota más baja en el corto plazo.
+      </>
+    ),
+    optionsButton: [
+      {
+        id: '1',
+        label: 'Entendido',
+        action: () => {
+          setIsOpenInfo(false)
+        },
+      },
+    ],
+  }
   return (
     <>
       <BtnPrimary onClick={() => setIsOpen(true)}>Ejemplo modal</BtnPrimary>
+      <br />
+      <BtnPrimary onClick={() => setIsOpenInfo(true)}>Ejemplo modal Info</BtnPrimary>
       <ModalAlert {...optionsModal} />
+      <ModalAlert {...optionsModaInfol} />
     </>
   )
 }
