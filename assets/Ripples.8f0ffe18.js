7687289d416393b7ec628b67d94ddce1bc1dbc07
Jonathangarrido <Jonathangarrido@users.noreply.github.com>
2024-04-08 21:54:56 +0000

    deploy: 0703e974d3ce668e29038e7fb186ca72088ec462

---
+++ assets/Ripples.8f0ffe18.js
@@ -0,0 +1,2 @@
+import{R as p}from"./Close.370ec775.js";import{a as r,B as s}from"./index.a77ec79a.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.8f0ffe18.js.map
