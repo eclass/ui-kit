2e0ed3959e3051c590c361f9e722e92d7b59fbc4
Eriold <Eriold@users.noreply.github.com>
2024-07-09 16:54:01 +0000

    deploy: c421c59f4feefc7373023f34b013b7e41bbd8902

--- assets/Instalation.6126312e.js
+++
@@ -1,9 +0,0 @@
-import{j as r,F as i,a as e,M as n,b as a,C as t,c as s}from"./index.a77ec79a.js";const m=()=>r(i,{children:[e(n,{children:"Instalaci\xF3n"}),e(a,{children:"Se debe instalar este repositorio m\xE1s unas dependencias"}),e(t,{language:"text",text:"npm install @eclass/ui-kit @chakra-ui/react @emotion/react @emotion/styled framer-motion"}),e(s,{children:"Implementaci\xF3n"}),e(a,{children:"Ejemplo de implementaci\xF3n en c\xF3digo."}),e(t,{text:`import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '@eclass/ui-kit'
-
-const AppWrapper = () => (
-<ChakraProvider theme={theme} resetCSS={false}>
-  <App />
-</ChakraProvider>
-)`})]});export{m as Instalation,m as default};
-//# sourceMappingURL=Instalation.6126312e.js.map
