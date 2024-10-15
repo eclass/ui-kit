9cb75129a0f99e0a4f45ec96d52f08b074004202
Eriold <Eriold@users.noreply.github.com>
2024-10-15 17:12:13 +0000

    deploy: 7885994789b60033ac96fdf82eb78bb259dd48fe

---
+++ assets/Events.dc9a8c68.js
@@ -0,0 +1,39 @@
+import{j as i,Q as b,a as e,a7 as E,a2 as T,B as n,v as o,T as r,F as A,M as S,b as s,c as v,C as f}from"./index.454bd3b4.js";const y=({color:t="white"})=>i(b,{width:"16px",height:"17px",viewBox:"0 0 16 17",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e("path",{d:"M15 2.5H13V0.5H11V2.5H9V0.5H7V2.5H5V0.5H3V2.5H1C0.447 2.5 0 2.947 0 3.5V15.5C0 16.053 0.447 16.5 1 16.5H15C15.553 16.5 16 16.053 16 15.5V3.5C16 2.947 15.553 2.5 15 2.5ZM14 14.5H2V5.5H14V14.5Z",fill:t}),e("path",{d:"M6 7.5H4V9.5H6V7.5Z",fill:t}),e("path",{d:"M9 7.5H7V9.5H9V7.5Z",fill:t}),e("path",{d:"M6 10.5H4V12.5H6V10.5Z",fill:t}),e("path",{d:"M9 10.5H7V12.5H9V10.5Z",fill:t}),e("path",{d:"M12 7.5H10V9.5H12V7.5Z",fill:t}),e("path",{d:"M12 10.5H10V12.5H12V10.5Z",fill:t})]}),V=({color:t="white"})=>e(b,{width:"16px",height:"17px",viewBox:"0 0 16 17",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:e("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M0 8.5C0 4.1 3.6 0.5 8 0.5C12.4 0.5 16 4.1 16 8.5C16 12.9 12.4 16.5 8 16.5C3.6 16.5 0 12.9 0 8.5ZM7 9.5H12V7.5H9V4.5H7V9.5Z",fill:t})}),w=({id:t,eventTitle:a,startDate:h,startTime:x,buttonInfo:g,redirect:u,eventImage:m,targetBlank:d,isMobile:l})=>e(E,{href:u,_hover:{textDecoration:"none"},target:d?"_blank":"_self",rel:d?"noopener noreferrer":void 0,display:"inline-block",children:i(T,{cursor:"pointer",transition:"box-shadow 0.3s ease-in-out",_hover:{shadow:"sm"},w:"fit-content",borderRadius:"8px",display:l?"block":"flex",children:[e(n,{width:"285px",height:l?"140px":"168px",bgImage:`linear-gradient(to bottom, rgba(0,0,0,0) 35%, rgba(0,0,0,1) 100%), url(${m})`,backgroundSize:"cover",borderRadius:l?"8px 8px 0px 0px":"8px 0 0 8px"}),i(n,{width:l?"285px":"305px",minHeight:l?"160px":"168px",borderRadius:l?"0 0 8px 8px":"0 8px 8px 0",border:"1px",borderColor:o("colors-neutral-platinum"),display:"flex",flexDirection:"column",cursor:"pointer",children:[i(n,{width:"inherit",height:"100%",padding:"4",color:"#555555",gap:"16px",display:"grid",children:[e(r,{fontWeight:"700",children:a}),i(n,{display:"flex",alignItems:"center",gap:"4px",children:[e(y,{color:o("colors-main-ziggurat")}),e(r,{fontWeight:"400",children:h}),e(r,{color:o("colors-neutral-platinum"),children:"|"}),e(V,{color:o("colors-main-ziggurat")}),e(r,{children:x})]})]}),e(n,{borderTop:`1px solid ${o("colors-neutral-platinum")}`,width:"100%",height:"50px",padding:"12px",backgroundColor:"white",alignItems:"center",display:"flex",justifyContent:"center",borderRadius:" 0 0 8px 8px",fontSize:"16px",fontFamily:"Roboto",color:o("colors-main-deepSkyBlue"),fontWeight:"500",children:g})]})]})},t),H=({id:t,eventTitle:a,startDate:h,startTime:x,buttonInfo:g,redirect:u,eventImage:m,targetBlank:d})=>e(E,{href:u,_hover:{textDecoration:"none"},target:d?"_blank":"_self",rel:d?"noopener noreferrer":void 0,display:"inline-block",children:i(n,{width:"286px",height:"247px",borderRadius:"8px",border:"1px",borderColor:o("colors-neutral-platinum"),justifyContent:"center",textAlign:"center",display:"flex",flexDirection:"column",cursor:"pointer",transition:"box-shadow 0.3s ease-in-out",_hover:{shadow:"sm"},children:[e(n,{flex:"1",bgImage:`linear-gradient(to bottom, rgba(0,0,0,0) 35%, rgba(0,0,0,1) 100%), url(${m})`,backgroundSize:"cover",borderRadius:"8px 8px 0 0",margin:"0",display:"grid",alignItems:"flex-end",children:i(n,{color:o("colors-neutral-white"),padding:"16px",textAlign:"initial",fontFamily:"Roboto",fontSize:"16px",lineHeight:"21px",display:"grid",gap:"8px",children:[e(r,{fontWeight:"700",children:a}),i(n,{display:"flex",alignItems:"center",gap:"4px",children:[e(y,{}),e(r,{fontWeight:"400",children:h}),e(r,{children:"|"}),e(V,{}),e(r,{children:x})]})]})}),e(n,{width:"100%",height:"50px",backgroundColor:"white",alignItems:"center",display:"flex",justifyContent:"center",borderRadius:" 0 0 8px 8px",fontSize:"16px",fontFamily:"Roboto",color:o("colors-main-deepSkyBlue"),fontWeight:"500",children:g})]})},t),p=({typeEvent:t,itemsEvent:a})=>t==="LARGE_EVENTS"?e(w,{...a}):e(H,{...a}),c=[{typeEvent:"LARGE_EVENTS",itemsEvent:{id:"1",eventTitle:"Resuelve tus dudas en vivo | Martes",startDate:"04/27/2024",startTime:"14:00 hrs",buttonInfo:"Ver informaci\xF3n del evento",redirect:"https://.....",eventImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT76y4DAhqWAlCspmGYWcbxSDnLw3p1FzJQKA&usqp=CAU",isMobile:!1}},{typeEvent:"LARGE_EVENTS",itemsEvent:{id:"1",eventTitle:"Resuelve tus dudas en vivo | Martes",startDate:"04/27/2024",startTime:"14:00 hrs",buttonInfo:"Ver informaci\xF3n del evento",redirect:"https://.....",eventImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT76y4DAhqWAlCspmGYWcbxSDnLw3p1FzJQKA&usqp=CAU",isMobile:!0}},{typeEvent:"SMALL_EVENTS",itemsEvent:{id:"1",eventTitle:"Resuelve tus dudas en vivo | Martes",startDate:"04/27/2024",startTime:"14:00 hrs",buttonInfo:"Ver informaci\xF3n del evento",redirect:"https://.....",eventImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT76y4DAhqWAlCspmGYWcbxSDnLw3p1FzJQKA&usqp=CAU",isMobile:!0}}],L=()=>i(A,{children:[e(S,{children:"Eventos"}),i(s,{children:["Los eventos estan contenidos en una nueva caja, la cual cuenta con 3 disenios distintos agrupados en dos tipos",e("strong",{children:" LARGE_EVENTS"})," y ",e("strong",{children:" SMALL_EVENTS."}),"LARGE_EVENTS: contiene un responsive para mobile y desktop."]}),e(v,{children:"Types Definidos"}),e(s,{children:"El componente Eventos requiere el siguiente typado:"}),e(f,{text:`export interface EventosProps {
+typeEvent: 'SMALL_EVENTS' | 'LARGE_EVENTS' | string // Tipo de caja
+isMobile?: boolean // Si es mobile o no
+itemsEvent: ItemsEventsProps // Objeto de eventos
+}
+
+export interface ItemsEventsProps {
+id: string // Key para ordenar
+eventTitle: string // Titulo del evento
+startDate: string // Fecha de inicio del evento
+startTime: string // Hora de inicio del evento
+buttonInfo: string // Texto del boton
+redirect: string // Link de redireccion
+eventImage: string // Imagen del evento
+targetBlank?: boolean // Si abre en nueva pesta\xF1a
+isMobile?: boolean // Si es mobile o no
+}
+
+`}),e("i",{children:"Se deja en espanol debido a que Event es una funcion nativa"}),e(f,{text:"import { Eventos } from '@eclass/ui-kit'"}),e(p,{...c[2]}),e(v,{children:"Estados"}),e(s,{children:"Existen 3 posibles estados que definen el dise\xF1o del evento."}),i(n,{display:"grid",gap:"16px",children:[e(s,{children:e("strong",{children:"LARGE_EVENTS"})}),e(p,{...c[0]}),e(s,{children:e("strong",{children:"LARGE_EVENTS para Mobile"})}),e(p,{...c[1]}),e(s,{children:e("strong",{children:"SMALL_EVENTS o por defecto"})}),e(p,{...c[2]})]}),e(v,{children:"Modo de uso"}),e(s,{children:"El Evento se utiliza de manera bastante simple siguiendo el siguiente ejemplo:"}),e(f,{text:`// importamos el componente
+import { Eventos } from '@eclass/ui-kit'
+//Definimos las propieaddes para el Evento
+const dataFakeEvents = {
+{
+typeEvent: 'LARGE_EVENTS',
+itemsEvent: {
+id: '1',
+eventTitle: 'Resuelve tus dudas en vivo | Martes',
+startDate: '04/27/2024',
+startTime: '14:00 hrs',
+buttonInfo: 'Ver informaci\xF3n del evento',
+redirect: 'https://.....',
+eventImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT76y4DAhqWAlCspmGYWcbxSDnLw3p1FzJQKA&usqp=CAU',
+isMobile: false,
+},
+}
+} // Utilizamos el componente
+<Eventos {...dataFakeEvents} />
+`}),e(p,{...c[0]})]});export{L as default};
+//# sourceMappingURL=Events.dc9a8c68.js.map
