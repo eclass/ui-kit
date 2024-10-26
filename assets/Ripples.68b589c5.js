2c0be63251b143d03e7552a1a76012cdb3dcc0b7
Eriold <Eriold@users.noreply.github.com>
2024-10-26 01:33:56 +0000

    deploy: 89a32363dd7913e14b73beda8f29149f937a9709

---
+++ assets/Ripples.68b589c5.js
@@ -0,0 +1,2 @@
+import{R as p}from"./Close.04874f9c.js";import{a as r,B as s}from"./index.8e73cbc0.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.68b589c5.js.map
--- assets/Ripples.d1990688.js
+++
@@ -1,2 +0,0 @@
-import{R as p}from"./Close.14209915.js";import{a as r,B as s}from"./index.8b485190.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
-//# sourceMappingURL=Ripples.d1990688.js.map
