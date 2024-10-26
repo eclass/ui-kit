2893e69ea3f2b052fbf7e2d8b54ef02b6105d6d0
Daniel Montoya <daniel.montoya@eclass.cl>
2024-10-25 20:19:58 -0500

    fix(PCV-1094): se corrige hover caja ripple para eventos y colores

--- src/organisms/Events/TypeBox/SmallBox.tsx
+++ src/organisms/Events/TypeBox/SmallBox.tsx
@@ -3,6 +3,7 @@ import { Box, Text, Link } from '@chakra-ui/react'
 import { vars } from '@/theme'
 import { ItemsEventsProps } from '../EventsTypes'
 import { Calendar, Clock } from '../Icons'
+import { Ripples } from '@/atoms'
 
 export const SmallBox: FC<ItemsEventsProps> = ({
   id,
@@ -21,75 +22,78 @@ export const SmallBox: FC<ItemsEventsProps> = ({
     <Link
       key={id}
       href={redirect}
-      _hover={{ textDecoration: 'none' }}
       target={targetBlank ? '_blank' : '_self'}
       rel={targetBlank ? 'noopener noreferrer' : undefined}
-      display="contents"
+      transition="box-shadow .3s"
+      _active={cssActive}
+      _hover={cssActive}
+      _focus={cssActive}
+      width="286px"
+      height="247px"
+      borderRadius={vars('radii-big')}
     >
-      <Box
-        width="286px"
-        height="247px"
-        borderRadius={vars('radii-big')}
-        border={vars('borders-light')}
-        justifyContent="center"
-        textAlign="center"
-        display="flex"
-        flexDirection="column"
-        cursor="pointer"
-        transition="box-shadow .3s"
-        _active={cssActive}
-        _hover={cssActive}
-        _focus={cssActive}
-      >
+      <Ripples>
         <Box
-          flex="1"
-          bgImage={`linear-gradient(to bottom, rgba(0,0,0,0) 35%, rgba(0,0,0,1) 100%), url(${eventImage})`}
-          backgroundSize="cover"
-          borderRadius="8px 8px 0 0"
-          margin="0"
-          display="grid"
-          alignItems="flex-end"
+          width="286px"
+          height="247px"
+          borderRadius={vars('radii-big')}
+          border={vars('borders-light')}
+          justifyContent="center"
+          textAlign="center"
+          display="flex"
+          flexDirection="column"
+          cursor="pointer"
         >
           <Box
-            color={vars('colors-neutral-white')}
-            padding="16px"
-            textAlign="initial"
-            fontFamily="Roboto"
-            fontSize="16px"
-            lineHeight="21px"
+            flex="1"
+            bgImage={`linear-gradient(to bottom, rgba(0,0,0,0) 35%, rgba(0,0,0,1) 100%), url(${eventImage})`}
+            backgroundSize="cover"
+            borderRadius="8px 8px 0 0"
+            margin="0"
             display="grid"
-            gap="8px"
+            alignItems="flex-end"
           >
-            <Text fontWeight="700" m="0px">
-              {eventTitle}
-            </Text>
-            <Box display="flex" alignItems="center" gap="4px">
-              <Calendar />
-              <Text fontWeight="400" m="0px">
-                {startDate}
+            <Box
+              color={vars('colors-neutral-white')}
+              padding="16px"
+              textAlign="initial"
+              fontFamily="Roboto"
+              fontSize="16px"
+              lineHeight="21px"
+              display="grid"
+              gap="8px"
+            >
+              <Text fontWeight="700" m="0px">
+                {eventTitle}
               </Text>
-              <Text m="0px">|</Text>
-              <Clock />
-              <Text m="0px">{startTime}</Text>
+              <Box display="flex" alignItems="center" gap="4px">
+                <Calendar />
+                <Text fontWeight="400" m="0px">
+                  {startDate}
+                </Text>
+                <Text m="0px">|</Text>
+                <Clock />
+                <Text m="0px">{startTime}</Text>
+              </Box>
             </Box>
           </Box>
+          <Box
+            width="100%"
+            height="50px"
+            backgroundColor="white"
+            alignItems="center"
+            display="flex"
+            justifyContent="center"
+            borderRadius=" 0 0 8px 8px"
+            fontSize="16px"
+            fontFamily="Roboto"
+            color={vars('colors-main-deepSkyBlue')}
+            fontWeight="500"
+          >
+            {buttonInfo}
+          </Box>
         </Box>
-        <Box
-          width="100%"
-          height="50px"
-          backgroundColor="white"
-          alignItems="center"
-          display="flex"
-          justifyContent="center"
-          borderRadius=" 0 0 8px 8px"
-          fontSize="16px"
-          fontFamily="Roboto"
-          color={vars('colors-main-deepSkyBlue')}
-          fontWeight="500"
-        >
-          {buttonInfo}
-        </Box>
-      </Box>
+      </Ripples>
     </Link>
   )
 }
