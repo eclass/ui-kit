45919e262f64241803263fd05a8bb26bfb486601
JaviMunita <JaviMunita@users.noreply.github.com>
2024-11-07 14:37:16 +0000

    deploy: 2ce8073cc39ba1e7e2618563e3070588b3282317

---
+++ assets/Ripples.88472b6e.js
@@ -0,0 +1,2 @@
+import{R as p}from"./Close.2d6e0bce.js";import{a as r,B as s}from"./index.669d2ae8.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.88472b6e.js.map
