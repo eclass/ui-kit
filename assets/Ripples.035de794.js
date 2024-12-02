c41df7174b9fdbd9c9cec57c9c50ca725169ebe9
Eriold <Eriold@users.noreply.github.com>
2024-12-02 19:06:14 +0000

    deploy: f87dcb94a9a55c7dba52697050894c32b72fa014

--- assets/Ripples.035de794.js
+++
@@ -1,2 +0,0 @@
-import{R as p}from"./DiagonalArrow.83142357.js";import{a as r,B as s}from"./index.ad3fe227.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
-//# sourceMappingURL=Ripples.035de794.js.map
---
+++ assets/Ripples.992f2ec3.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.23296175.js";import{a as r,B as s}from"./index.228d91b7.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.992f2ec3.js.map
