07f12e40436e3192ce70f64e9a2ed652fa367fe4
Javiera Munita <javiera.munita@eclass.cl>
2025-03-04 19:40:43 -0300

    fix(PDYE-1116): botón que abre menú

--- src/organisms/Calendar/Dropdown/CalendarDropdown/Components/GoToCalendar.tsx
+++ src/organisms/Calendar/Dropdown/CalendarDropdown/Components/GoToCalendar.tsx
@@ -5,22 +5,31 @@ import { CalendarButtonIcon } from '../Icons/CalendarButtonIcon'
 import { NotificationIcon } from '../../../EventsList/NotificationIcon'
 import { NewTooltip } from '@/molecules';
 
-export const GoToCalendar = ({ hasNew, text, tooltipDisabled }: { hasNew: boolean; text: string, tooltipDisabled: boolean }) => {
+interface IGoToCalendar {
+  hasNew: boolean
+  text: string
+  tooltipDisabled: boolean
+}
+
+export const GoToCalendar = ({ hasNew, text, tooltipDisabled }:IGoToCalendar ) => {
   return (
-    <NewTooltip label={text ?? 'Calendario'} m="8px 0 0 0" isDisabled={tooltipDisabled}>
+    <NewTooltip label={text ?? 'Calendario'} m="2px 0 0 0" isDisabled={tooltipDisabled}>
       <MenuButton
         background="#60798E"
         border="1px solid transparent"
         borderRadius="100%"
         height="30px"
         width="30px"
+        position="relative"
         sx={{
           '>span': {
-            height: '30px',
-            padding: '1px 0',
+            justifyItems: 'center',
+            alignContent: 'center',
           },
           '.notification': {
             position: 'absolute',
+            top: '-1px',
+            right: '-1px'
           },
         }}>
         <CalendarButtonIcon />
---
+++ src/organisms/Calendar/EventsList/NotificationIcon.tsx
@@ -0,0 +1,5 @@
+export const NotificationIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={8} height={9} fill="none" className="notification">
+    <circle cx={4} cy={4.5} r={4} fill="#FF7A14" />
+  </svg>
+)
