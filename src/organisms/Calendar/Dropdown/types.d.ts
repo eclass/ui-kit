ae515fe37224c7f087399dee3b9f4c9331f1695d
Javiera Munita <javiera.munita@eclass.cl>
2025-03-05 09:39:07 -0300

    feat(PDYE-1119): format y package-lock

--- src/organisms/Calendar/Dropdown/types.d.ts
+++ src/organisms/Calendar/Dropdown/types.d.ts
@@ -1,27 +1,25 @@
 interface AssociatedResource {
-    id: number
-  }
-  
-  interface Course {
-    id: number
-    name: string
-  }
-  
-  interface FormattedDate {
-    start: string
-    hours: string
-  }
-  
-  interface Event {
-    id: number
-    course_id: number
-    start: string
-    end: string
-    associated_resource: AssociatedResource
-    course: Course
-    formatedDate: FormattedDate
-  }
-  
+  id: number
+}
+
+interface Course {
+  id: number
+  name: string
+}
+
+interface FormattedDate {
+  start: string
+  hours: string
+}
+
+interface Event {
+  id: number
+  course_id: number
+  start: string
+  end: string
+  associated_resource: AssociatedResource
+  course: Course
+  formatedDate: FormattedDate
+}
+
 export type Events = Event[]
-  
-  
