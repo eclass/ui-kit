8a79c75ba753a769b8e58749d4390453709a39c7
Eriold <Eriold@users.noreply.github.com>
2025-03-05 21:43:08 +0000

    deploy: 8f1e17513705b0af07e1b4ca2571c4558da00f24

---
+++ assets/Ripples.008e1fc5.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.66be41e8.js";import{a as r,B as s}from"./index.209bf9a3.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.008e1fc5.js.map
--- assets/Ripples.67223a95.js
+++
@@ -1,2 +0,0 @@
-import{R as p}from"./DiagonalArrow.348441de.js";import{a as r,B as s}from"./index.5c52a36f.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
-//# sourceMappingURL=Ripples.67223a95.js.map
