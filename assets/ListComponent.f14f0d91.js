f0cfccb7925bfa8861f79f8d52243f2a06c5237b
Eriold <Eriold@users.noreply.github.com>
2025-03-06 02:05:43 +0000

    deploy: 69ec001efdd32566b3e55abd8b54e7aafcc189b0

---
+++ assets/ListComponent.f14f0d91.js
@@ -0,0 +1,2 @@
+import{a as e,B as n}from"./index.48503c60.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
+//# sourceMappingURL=ListComponent.f14f0d91.js.map
