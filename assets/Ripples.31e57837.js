a223156d94fdae772cf5dd1dee158a5ba3d10f9f
Jonathangarrido <Jonathangarrido@users.noreply.github.com>
2025-03-10 22:08:28 +0000

    deploy: 8e9a71a2aedbbba0ba2289ed2ea01df22fba151e

---
+++ assets/Ripples.31e57837.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.95099795.js";import{a as r,B as s}from"./index.98286509.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.31e57837.js.map
