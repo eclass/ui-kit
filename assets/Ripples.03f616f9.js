8951d87fa6d8f0a386c09524b4f9d3e7fe1b4e7e
JaviMunita <JaviMunita@users.noreply.github.com>
2025-03-10 18:18:33 +0000

    deploy: c2e8b4d5b111130ed9f652c40c9dced78eaca1ba

---
+++ assets/Ripples.03f616f9.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.67c4ec3f.js";import{a as r,B as s}from"./index.b00f764d.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.03f616f9.js.map
--- assets/Ripples.d1a47c13.js
+++
@@ -1,2 +0,0 @@
-import{R as p}from"./DiagonalArrow.dbd65cce.js";import{a as r,B as s}from"./index.9db5f18e.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
-//# sourceMappingURL=Ripples.d1a47c13.js.map
