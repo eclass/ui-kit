1b006b0099a39f292e19055c3bec1ea0e162375b
JaviMunita <JaviMunita@users.noreply.github.com>
2025-03-10 17:36:18 +0000

    deploy: 267df3612e0564930d4b8fa365e5d2bd969d75e2

--- assets/ListComponent.46c7e64c.js
+++
@@ -1,2 +0,0 @@
-import{a as e,B as n}from"./index.23e94469.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
-//# sourceMappingURL=ListComponent.46c7e64c.js.map
---
+++ assets/ListComponent.8efdc686.js
@@ -0,0 +1,2 @@
+import{a as e,B as n}from"./index.9db5f18e.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
+//# sourceMappingURL=ListComponent.8efdc686.js.map
