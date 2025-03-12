98370d8a5cfbf8d835a4e27a5f289c8bb36c2f65
Jonathangarrido <Jonathangarrido@users.noreply.github.com>
2025-03-12 19:29:43 +0000

    deploy: a81f77dde9d885e9cad2b2587a770042e5c75c2b

---
+++ assets/TinyAlert.d6d1856c.js
@@ -0,0 +1,10 @@
+import{j as s,F as n,a as e,M as r,b as a,C as i,c as o}from"./index.1a6c8d2b.js";import"./DiagonalArrow.28a74e5d.js";import{T as t}from"./TinyAlert.f53b727f.js";import{L as d}from"./ListComponent.82ea90b7.js";const m=()=>s(n,{children:[e(r,{children:"TinyAlert"}),e(a,{children:"Este componente permite resaltar y mostrar informaci\xF3n importante de una manera visualmente atractiva. Son ideales para realzar elementos espec\xEDficos, como notificaciones, etiquetas de estado, o cualquier otro dato que desees destacar."}),e(t,{status:"success"}),e(i,{text:'<TinyAlert status="success" />'}),e(o,{children:"Status"}),e(a,{children:"El componente ofrece una variedad de tipos predefinidos que se adaptan a diferentes necesidades de dise\xF1o. Cada tipo tiene su propio prop\xF3sito y apariencia, lo que te permite personalizar a\xFAn m\xE1s la presentaci\xF3n de la informaci\xF3n destacada. Aqu\xED est\xE1n los tipos disponibles:"}),s(d,{children:[e(t,{status:"success"}),e(t,{status:"error"}),e(t,{status:"info"}),e(t,{status:"warning"}),e(t,{status:"answered"}),e(t,{status:"pending"}),e(t,{status:"omitted"}),e(t,{status:"new"}),e(t,{status:"success",text:"Texto personalizado"})]}),e(i,{text:`<TinyAlert status="success" />
+<TinyAlert status="error" />
+<TinyAlert status="info" />
+<TinyAlert status="warning" />
+<TinyAlert status="answered" />
+<TinyAlert status="pending" />
+<TinyAlert status="omitted" />
+<TinyAlert status="new" />
+<TinyAlert status="success" text="Texto personalizado" />`})]});export{m as ViewTinyAlert,m as default};
+//# sourceMappingURL=TinyAlert.d6d1856c.js.map
