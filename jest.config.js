ab8b4e2f294c52f08c691d799251ae392e252f55
Daniel Camilo Montoya <daniel2camilo2018@gmail.com>
2025-03-13 08:08:08 -0500

    fix(): se corrige la exportacion de types para versiones futuras (#695)
    Co-authored-by: Daniel Montoya <daniel.montoya@eclass.cl>

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,21 @@
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig')
+const { pathsToModuleNameMapper } = require('ts-jest');
+const fs = require('fs');
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// Verifica que tsconfig.json existe antes de requerirlo
+const tsConfig = fs.existsSync('./tsconfig.json') ? require('./tsconfig.json') : {};
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test/jest-setup.ts'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   roots: ['<rootDir>'],
-  modulePaths: [compilerOptions.baseUrl],
+  modulePaths: tsConfig.compilerOptions ? [tsConfig.compilerOptions.baseUrl] : [],
   moduleNameMapper: {
-    ...pathsToModuleNameMapper(compilerOptions.paths), 
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css)$": "identity-obj-proxy"
+    ...(tsConfig.compilerOptions && tsConfig.compilerOptions.paths
+      ? pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>/' })
+      : {}),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css)$': 'identity-obj-proxy'
   }
-}
+};
