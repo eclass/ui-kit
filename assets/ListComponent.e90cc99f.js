da5ad94d7c466be77e5164a3612f6babd19409be
Eriold <Eriold@users.noreply.github.com>
2025-03-13 14:09:06 +0000

    deploy: 408ddbbea8b25760a107261d3411844b96668a0b

--- assets/ListComponent.e90cc99f.js
+++
@@ -1,2 +0,0 @@
-import{a as e,B as n}from"./index.6e4f33f0.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
-//# sourceMappingURL=ListComponent.e90cc99f.js.map
---
+++ assets/Ripples.a3401169.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.42848f08.js";import{a as r,B as s}from"./index.dce1bca6.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.a3401169.js.map
