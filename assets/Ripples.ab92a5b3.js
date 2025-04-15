f935a79fac3e1fdf855053d9fb1208f6d452afdc
Eriold <Eriold@users.noreply.github.com>
2025-04-15 21:35:41 +0000

    deploy: 9fdffc36e4311ce3ae9d0d37f05994bac2d20a82

---
+++ assets/Ripples.ab92a5b3.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.5597c8d1.js";import{a as r,B as s}from"./index.adb27c8b.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.ab92a5b3.js.map
