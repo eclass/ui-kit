bc3e08326010dfa4b8d9f532a04d72acb39b3091
Daniel Camilo Montoya <daniel2camilo2018@gmail.com>
2025-04-15 17:00:41 -0500

    fix(): se hace reduccin del bundle probando la configuracion sugerida (#704)
    Co-authored-by: Daniel Montoya <daniel.montoya@eclass.cl>

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,34 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
-// https://vitejs.dev/config/
-
-let myDefineConfig
-
-const buildEnv = process.env.VITE_REACT_DEPLOY_DOCUMENTATION
-
-if (buildEnv === 'DOCUMENTATION') {
-  myDefineConfig = {
-    plugins: [react(), tsconfigPaths()],
-    build: {
-      sourcemap: true,
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@eclass/ui-kit',
+      fileName: (format: string) => `eclass-ui-kit.${format}.js`,
     },
+    sourcemap: true,
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        '@chakra-ui/react',
+        '@emotion/react',
+        '@emotion/styled',
+        'framer-motion'
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@chakra-ui/react': 'Chakra',
+          '@emotion/react': 'EmotionReact',
+          '@emotion/styled': 'EmotionStyled',
+          'framer-motion': 'framerMotion'
+        }
+      }
+    }
   }
-} else {
-  myDefineConfig = {
-    plugins: [react(), tsconfigPaths()],
-    build: {
-      lib: {
-        entry: path.resolve(__dirname, 'src/index.ts'),
-        name: '@eclass/ui-kit',
-        fileName: (format: string) => `eclass-ui-kit.${format}.js`,
-      },
-      sourcemap: true,
-      rollupOptions: {
-        // make sure to externalize deps that shouldn't be bundled
-        // into your library
-        external: ['react'],
-        output: {
-          // Provide global variables to use in the UMD build
-          // for externalized deps
-          globals: {
-            react: 'React',
-          },
-        },
-      },
-    },
-  }
-}
-
-export default defineConfig(myDefineConfig)
+})
