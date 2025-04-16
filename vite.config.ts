6f4ecd49285e608e423bcecdf4853623b78c5cca
Daniel Camilo Montoya <daniel2camilo2018@gmail.com>
2025-04-16 09:48:15 -0500

    fix(): se agrega division de configuracion de vite para documentacion (#705)
    Co-authored-by: Daniel Montoya <daniel.montoya@eclass.cl>

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,51 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
+// VITE_REACT_DEPLOY_DOCUMENTATION viene de tu script "deploy"
+const isDocs = import.meta.env.VITE_REACT_DEPLOY_DOCUMENTATION === 'DOCUMENTATION'
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: '@eclass/ui-kit',
-      fileName: (format: string) => `eclass-ui-kit.${format}.js`,
-    },
-    sourcemap: true,
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        '@chakra-ui/react',
-        '@emotion/react',
-        '@emotion/styled',
-        'framer-motion'
-      ],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@chakra-ui/react': 'Chakra',
-          '@emotion/react': 'EmotionReact',
-          '@emotion/styled': 'EmotionStyled',
-          'framer-motion': 'framerMotion'
+    // Cuando sea docs, salida por defecto (dist/)
+    // y no usaremos el modo "lib"
+    ...(isDocs
+      ? {
+          // build de la web de componentes
+          outDir: 'dist-docs',
+          sourcemap: true,
+          rollupOptions: {
+            external: [],       // empaquetar todo
+          },
         }
-      }
-    }
-  }
+      : {
+          // build de la librería
+          lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: '@eclass/ui-kit',
+            fileName: (format: string) => `eclass-ui-kit.${format}.js`,
+          },
+          sourcemap: true,
+          rollupOptions: {
+            external: [
+              'react',
+              'react-dom',
+              '@chakra-ui/react',
+              '@emotion/react',
+              '@emotion/styled',
+              'framer-motion',
+            ],
+            output: {
+              globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM',
+                '@chakra-ui/react': 'Chakra',
+                '@emotion/react': 'EmotionReact',
+                '@emotion/styled': 'EmotionStyled',
+                'framer-motion': 'framerMotion',
+              },
+            },
+          },
+        }),
+  },
 })
