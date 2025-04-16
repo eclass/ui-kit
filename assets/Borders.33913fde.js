58fa10579d38a91cafee3bb8b91f54cc89ef0890
Eriold <Eriold@users.noreply.github.com>
2025-04-16 16:31:15 +0000

    deploy: 1b20a4672f1dd7d4fbed65a645743355fc5eb8cf

---
+++ assets/Borders.33913fde.js
@@ -0,0 +1,9 @@
+import{B as n}from"./BoxCopy.9bccd4f2.js";import{j as s,F as b,a as r,M as l,b as c,B as x,v as a,C as i,c as m}from"./index.c1464e53.js";import{L as t}from"./ListComponent.1607d7bb.js";const h=()=>s(b,{children:[r(l,{children:"Borders"}),r(c,{children:"Las variables CSS para tipos de bordes ofrecen una gama de colores predefinidos que pueden aplicarse a diferentes elementos. Esto te permite seleccionar r\xE1pidamente un color que se adapte al contexto y al prop\xF3sito de cada borde."}),s(t,{children:[r(e,{border:"borders-normal"}),r(e,{border:"borders-light"}),r(e,{border:"borders-active"}),r(e,{border:"borders-success"}),r(e,{border:"borders-error"}),r(x,{p:"1rem",bg:a("colors-neutral-darkCharcoal"),children:r(e,{border:"borders-white"})})]}),r(i,{text:`<Box border={vars('borders-normal')} />
+<Box border={vars('borders-light')} />
+<Box border={vars('borders-active')} />
+<Box border={vars('borders-success')} />
+<Box border={vars('borders-error')} />
+<Box border={vars('borders-white')} />`}),r(m,{children:"Radio"}),s(t,{children:[r(e,{borderRadius:"radii-small"}),r(e,{borderRadius:"radii-big"}),r(e,{borderRadius:"radii-rounded"})]}),r(i,{text:`<Box borderRadius={vars('radii-small')}/>
+<Box borderRadius={vars('radii-big')}/>
+<Box borderRadius={vars('radii-rounded')}/>`})]}),e=({borderRadius:o,border:d="borders-normal"})=>r(n,{h:"5.5rem",w:"5.5rem",border:a(d),borderRadius:a(o!=null?o:"0"),text:o!=null?o:d});export{h as ViewBorders,h as default};
+//# sourceMappingURL=Borders.33913fde.js.map
