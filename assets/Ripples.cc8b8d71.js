6dc6af25bfa2a83a4b34185adef926301c7a8e73
JaviMunita <JaviMunita@users.noreply.github.com>
2025-06-18 15:51:33 +0000

    deploy: 8606bf3f3ebab204f4230eb2ff28f338bbe60161

---
+++ assets/Ripples.cc8b8d71.js
@@ -0,0 +1,2 @@
+import{R as p}from"./DiagonalArrow.68a4055a.js";import{a as r,B as s}from"./index.8efe461b.js";function n({children:i,color:o="rgba(204,204,204,0.2)"}){return r(p,{during:1e3,color:o,children:r(s,{children:i})})}export{n as R};
+//# sourceMappingURL=Ripples.cc8b8d71.js.map
