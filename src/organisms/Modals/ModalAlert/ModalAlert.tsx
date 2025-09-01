c585047789c3031eb8e5efa0368f5ef4fc86e37e
Javiera Munita <javiera.munita@eclass.cl>
2025-09-01 10:41:45 -0400

    fix(PDYE-1395): auto focus modal alert new

--- src/organisms/Modals/ModalAlert/ModalAlert.tsx
+++ src/organisms/Modals/ModalAlert/ModalAlert.tsx
@@ -32,6 +32,7 @@ export const ModalAlertNew = ({
         motionPreset="scale"
         onClose={onClose}
         closeOnEsc={type !== 'loading'}
+        autoFocus={false}
       >
         <ModalOverlay />
         <ModalContent
