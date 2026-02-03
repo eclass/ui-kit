15e1c8f1916295e2c463f83a506d75db05c7ddfd
Jonathangarrido <Jonathangarrido@users.noreply.github.com>
2026-02-03 13:16:29 +0000

    deploy: 70f594a08853ce861798129711587661145ccd25

---
+++ assets/ListComponent.5039771a.js
@@ -0,0 +1,2 @@
+import{a as e,B as n}from"./index.b8aab86f.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
+//# sourceMappingURL=ListComponent.5039771a.js.map
--- assets/ListComponent.93c7dfa7.js
+++
@@ -1,2 +0,0 @@
-import{a as e,B as n}from"./index.0ac1ece3.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
-//# sourceMappingURL=ListComponent.93c7dfa7.js.map
