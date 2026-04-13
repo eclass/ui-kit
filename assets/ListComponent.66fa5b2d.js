7f85d026f07f63ff72cab88b195028e29a7db88d
josefin-ferrada <josefin-ferrada@users.noreply.github.com>
2026-04-13 16:41:52 +0000

    deploy: 2f198aa9f4cfb5fa5dc847e76f97ec7b87f74ac0

--- assets/ListComponent.66fa5b2d.js
+++
@@ -1,2 +0,0 @@
-import{a as e,B as n}from"./index.65834767.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
-//# sourceMappingURL=ListComponent.66fa5b2d.js.map
