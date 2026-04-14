b8a472dcea86b27a8a3ee076d5b66735042d0497
josefin-ferrada <josefin-ferrada@users.noreply.github.com>
2026-04-14 17:18:16 +0000

    deploy: 0171581e89841865dbaf7a0f17b682ca35bbf68a

--- assets/ListComponent.9fcf5e0c.js
+++
@@ -1,2 +0,0 @@
-import{a as e,B as n}from"./index.caf13dbc.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
-//# sourceMappingURL=ListComponent.9fcf5e0c.js.map
