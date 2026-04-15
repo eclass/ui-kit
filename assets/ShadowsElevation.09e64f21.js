d18b4a17e7a7cfaef60502c3126721d75c609227
josefin-ferrada <josefin-ferrada@users.noreply.github.com>
2026-04-15 14:39:22 +0000

    deploy: 623afb6b8ef77a4967a82cad37a57593c3c5fa0a

---
+++ assets/ShadowsElevation.09e64f21.js
@@ -0,0 +1,4 @@
+import{B as e}from"./BoxCopy.839da60e.js";import{j as a,F as r,a as s,M as d,b as t,v as o,C as m}from"./index.21a6401a.js";import{L as n}from"./ListComponent.26c5e605.js";const l=()=>a(r,{children:[s(d,{children:"Shadows \xA7 Elevation"}),s(t,{children:"Algunos de nuestros componentes necesitan separarse del resto de la interfaz para ello se dispone de las sombras, que est\xE1n reservadas para estos componentes (por ejemplo, men\xFA desplegable o modales)."}),a(n,{gap:"5rem",m:"3rem 0 !important",children:[s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-sm"),text:"shadows-sm"}),s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-md"),text:"shadows-md"}),s(e,{h:"5.5rem",w:"5.5rem",boxShadow:o("shadows-lg"),text:"shadows-lg"})]}),s(m,{text:`<Box boxShadow={vars('shadows-sm')} />
+<Box boxShadow={vars('shadows-md')} />
+<Box boxShadow={vars('shadows-lg')} />`})]});export{l as ViewShadowsElevation,l as default};
+//# sourceMappingURL=ShadowsElevation.09e64f21.js.map
