06cef704b87cfa787a23b9c88432080604dbf741
Jonathangarrido <Jonathangarrido@users.noreply.github.com>
2026-06-10 16:46:18 +0000

    deploy: 97da33fda452ea9e58fade7ab404bb9ae322c26c

---
+++ assets/ListComponent.83803c35.js
@@ -0,0 +1,2 @@
+import{a as e,B as n}from"./index.b47cbdd8.js";const o=({children:r,gap:t="1rem",m:a="0"})=>e(n,{display:"flex",flexWrap:"wrap",alignItems:"center",m:a,gap:t,sx:{">*":{marginInlineStart:"0 !important"}},children:r});export{o as L};
+//# sourceMappingURL=ListComponent.83803c35.js.map
